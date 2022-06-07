1c5ea1f615803c7992bfd5aa44bd10d9b639b086
Beau Reescano <25470092+jsonjunkie@users.noreply.github.com>
2022-06-07 00:01:43 -0500

    add_people startup system

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,29 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::new().add_system(hello_world).run();
+    App::new()
+        .add_startup_system(add_people)
+        .add_system(hello_world)
+        .run();
 }
 
 fn hello_world() {
     println!("hello world!");
+}
+
+fn add_people(mut commands: Commands) {
+    commands
+        .spawn()
+        .insert(Person)
+        .insert(Name("Elaina Proctor".to_string()));
+    commands
+        .spawn()
+        .insert(Person)
+        .insert(Name("Renzo Hume".to_string()));
+    commands
+        .spawn()
+        .insert(Person)
+        .insert(Name("Zayna Nieves".to_string()));
 }
 
 #[derive(Component)]
